ffdc4a6c4050bff4da8eadb812ebebcad1184078
pilotpimpf <telschowjonas@gmail.com>
2022-08-12 19:41:51 +0200

    bat added

--- volumeregulator.py
+++ volumeregulator.py
@@ -4,52 +4,55 @@ from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
 import configparser
 import os
 
-dirname = os.path.dirname(__file__)
+def startup():
+    dirname = os.path.dirname(__file__)
+    config = configparser.ConfigParser()
 
-config = configparser.ConfigParser()
-
-try:
-    config.read(f"{dirname}/config.ini")
-except:
-    with open(f"{dirname}/config.ini", "w") as f:
-        f.write("""
-            [device]
-            vendor_id = 9025
-            product_id = 32823
-
-            [1]
-            name = Spotify
-            process = Spotify.exe
-
-            [2]
-            name = Discord
-            process = Discord.exe
-
-            [3]
-            name = Game
-            process = fornite.exe,SuperHexagon.exe
-
-            [4]
-            name = Chrome
-            process = chrome.exe
-        """)
-    
-    print("Created default config file")
-
-    config.read(f"{dirname}/config.ini")
-
-
-try:
-    gamepad = hid.device()
     try:
-        gamepad.open(int(config.get("device", "vendor_id")), int(config.get("device", "product_id")), config.get("device", "serial_number"))
+        config.read(f"{dirname}/config.ini")
     except:
-        gamepad.open(int(config.get("device", "vendor_id")), int(config.get("device", "product_id")))
-        print("No serial number used")
-    gamepad.set_nonblocking(True)
-except:
-    print("Failed to open Device")
-    exit()
+        with open(f"{dirname}/config.ini", "w") as f:
+            f.write("""
+                [device]
+                vendor_id = 9025
+                product_id = 32823
+
+                [1]
+                name = Spotify
+                process = Spotify.exe
+
+                [2]
+                name = Discord
+                process = Discord.exe
+
+                [3]
+                name = Game
+                process = fornite.exe,SuperHexagon.exe
+
+                [4]
+                name = Chrome
+                process = chrome.exe
+            """)
+        
+        print("Created default config file")
+
+        config.read(f"{dirname}/config.ini")
+
+
+    try:
+        gamepad = hid.device()
+        try:
+            gamepad.open(int(config.get("device", "vendor_id")), int(config.get("device", "product_id")), config.get("device", "serial_number"))
+        except:
+            gamepad.open(int(config.get("device", "vendor_id")), int(config.get("device", "product_id")))
+            print("No serial number used")
+        gamepad.set_nonblocking(True)
+
+        return config, gamepad
+    except:
+        print("Failed to open Device")
+        exit()
+    
 
 def convert(n):
     if n >= 128:
@@ -131,46 +134,66 @@ def getAudio(process):
                 return round(volume.GetMasterVolume() * 100)
     return 100
 
-lastX = getAudio(config.get("1", "process"))
-lastY = getAudio(config.get("2", "process"))
-lastZ = getAudio(config.get("3", "process"))
-lastT = getAudio(config.get("4", "process"))
+def main(config, gamepad):
+    lastX = getAudio(config.get("1", "process"))
+    lastY = getAudio(config.get("2", "process"))
+    lastZ = getAudio(config.get("3", "process"))
+    lastT = getAudio(config.get("4", "process"))
 
-print(f'{config.get("1", "name")}: {lastX}%  {config.get("2", "name")}: {lastY}%  {config.get("3", "name")}: {lastZ}%  {config.get("4", "name")}: {lastT}%')
+    print(f'{config.get("1", "name")}: {lastX}%  {config.get("2", "name")}: {lastY}%  {config.get("3", "name")}: {lastZ}%  {config.get("4", "name")}: {lastT}%')
+    
+    while True:
+        try:
+            report = gamepad.read(64)
+        except:
+            break
+        if report:
+            buttons = assign_buttons(report)
+            x = buttons['x']
+            if abs(lastX -x) >= 2:
+                if x >= 99: x = 100
+                elif x <= 1: x= 0
+                setAudio(config.get('1', 'process'), round(x/100, 2))
+                lastX = x
+                print(f"Set {config.get('1', 'name')} to {x}%")
 
-while True:
-    report = gamepad.read(64)
-    if report:
-        buttons = assign_buttons(report)
-        x = buttons['x']
-        if abs(lastX -x) >= 2:
-            if x >= 99: x = 100
-            elif x <= 1: x= 0
-            setAudio(config.get('1', 'process'), round(x/100, 2))
-            lastX = x
-            print(f"Set {config.get('1', 'name')} to {x}%")
+            y = buttons ['y']
+            if abs(lastY -y) >= 2:
+                if y >= 99: y = 100
+                elif y <= 1: y= 0
+                setAudio(config.get('2', 'process'), round(y/100, 2))
+                lastY = y
+                print(f"Set {config.get('2', 'name')} to {y}%")
 
-        y = buttons ['y']
-        if abs(lastY -y) >= 2:
-            if y >= 99: y = 100
-            elif y <= 1: y= 0
-            setAudio(config.get('2', 'process'), round(y/100, 2))
-            lastY = y
-            print(f"Set {config.get('2', 'name')} to {y}%")
+            z = buttons['z']
+            if abs(lastZ -z) >= 2:
+                if z >= 99: z = 100
+                elif z <= 1: z= 0
+                setAudio(config.get('3', 'process'), round(z/100, 2))
+                lastZ = z
+                print(f"Set {config.get('3', 'name')} to {z}%")
 
-        z = buttons['z']
-        if abs(lastZ -z) >= 2:
-            if z >= 99: z = 100
-            elif z <= 1: z= 0
-            setAudio(config.get('3', 'process'), round(z/100, 2))
-            lastZ = z
-            print(f"Set {config.get('3', 'name')} to {z}%")
+            t = buttons['t']
+            if abs(lastT -t) >= 2:
+                if t >= 99: t = 100
+                elif t <= 1: t= 0
+                setAudio(config.get('4', 'process'), round(t/100, 2))
+                lastT = t
+                print(f"Set {config.get('4', 'name')} to {t}%")
+        sleep(0.01)
+    reconnect()
 
-        t = buttons['t']
-        if abs(lastT -t) >= 2:
-            if t >= 99: t = 100
-            elif t <= 1: t= 0
-            setAudio(config.get('4', 'process'), round(t/100, 2))
-            lastT = t
-            print(f"Set {config.get('4', 'name')} to {t}%")
-    sleep(0.01)
+def reconnect():
+    while True:
+        try:
+            config, gamepad = startup()
+            break
+        except: 
+            sleep(1)
+    main(config, gamepad)
+    
+
+
+config, gamepad = startup()
+
+main(config, gamepad)
